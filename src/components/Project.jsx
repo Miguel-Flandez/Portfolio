0b40e923abbcad3f9b08d49122f3fa4d3f9c816e
Miguel Flandez <immfpnvs2@gmail.com>
2025-11-12 14:22:19 +0800

    changed font for project description

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -4,7 +4,7 @@ export default function Project({name, description, thumbnail}){
         <div id="container" className="w-[20vw] h-[50vh] shrink-0 border-white hover:border flex flex-col items-center justify-around rounded-md pt-2 px-2">
             <img src={thumbnail} alt="pokedex-thumbnail" />
             <span className="font-stack-sans-notch text-white">{name}</span>
-            <span className="text-[#8a8a8a]">{description}</span>
+            <span className="text-[#8a8a8a] font-mono">{description}</span>
         </div>
     )
 }
