368c5d4c3372381edd29de0da45257aea6f25eaf
Miguel Flandez <immfpnvs2@gmail.com>
2025-11-26 21:31:05 +0800

    changed font to bold and z-index to 10

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,12 @@
 export default function Navbar(){
 
     return(
-        <nav className="flex items-center justify-between text-white px-[5vw] py-[2vh] fixed z-1 w-screen">
+        <nav className="flex items-center justify-between text-white px-[5vw] py-[2vh] fixed z-10 w-screen bg-[#1a1a1a]">
             <div>
                 <span className="text-2xl font-sixtyfour">IMF</span>
             </div>
 
-            <div className="flex gap-[20vw]">
+            <div className="flex gap-[20vw] font-bold">
                 <a href="#intro">About</a>
                 <a href="#showcase">Projects</a>
                 <a href="#contacts">Contacts</a>
