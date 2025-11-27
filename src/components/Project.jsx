867aa04bc7ef7c55e9b8c667b6013ae67d0230e6
Miguel Flandez <immfpnvs2@gmail.com>
2025-11-27 16:33:54 +0800

    added hover styling to projects component

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,10 +1,11 @@
 export default function Project({name, description, thumbnail}){
     
     return(
-        <div id="container" className="w-[20vw] h-[50vh] shrink-0 border-white hover:border flex flex-col items-center justify-around rounded-md pt-2 px-2">
+        <div id="container" className="text-sm w-[20vw] h-[50vh] shrink-0 hover:bg-[rgba(100,100,100,0.2)] hover:-translate-y-1 transition-all  border border-transparent flex flex-col items-center gap-[1vw] rounded-md pt-[1vw] px-[1vw] cursor-pointer">
             <img src={thumbnail} alt="pokedex-thumbnail" />
             <span className="font-stack-sans-notch text-white">{name}</span>
             <span className="text-[#8a8a8a] font-mono">{description}</span>
+            
         </div>
     )
 }
