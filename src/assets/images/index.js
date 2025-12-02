c591a410dc4f6679f11677a368625297ed2430ac
Miguel Flandez <immfpnvs2@gmail.com>
2025-12-02 15:40:08 +0800

    added thumbnail for pomodoro app

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,3 +1,4 @@
 import pokedexThumbnail from './pokedex-thumbnail.png'
+import pomodoroThumbnail from './pomodoro-thumbnail.png'
 
-export {pokedexThumbnail}
+export {pokedexThumbnail, pomodoroThumbnail}
